86e0a52b4f9cab5ce6f00fb1ac3cfbf51bfe0869
Gregory Balay <42926397+shaolynne@users.noreply.github.com>
2022-01-21 20:24:36 +0100

    Add files via upload

---
+++ boutique exam/footer.php
@@ -0,0 +1,17 @@
+
+	</div>
+</div>
+
+<div id="footer">
+<div class="wrap">
+<p>© Harmonie shop, a small ecommerce template</p>
+</div>
+</div>
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/app.js"></script>
+
+
+</body>
+
+</html>
